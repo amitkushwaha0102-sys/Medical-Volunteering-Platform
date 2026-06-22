07506546731bf3c8986752efbed6f192a9fc29ed
Amit Kushwaha <amitkushwaha0102@gmail.com>
2026-06-22 08:48:40 +0000

    test commit on feature branch

--- backend/app.py
+++ backend/app.py
@@ -14,4 +14,4 @@ app.register_blueprint(routes)
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)// Test comment
